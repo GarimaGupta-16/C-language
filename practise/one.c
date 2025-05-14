58c77b5ea10da28d2be73d7f8265e871cdefd295
Garima Gupta <garima0277.becse24@chitkara.in>
2025-05-14 20:27:56 +0530

    number right angled triangle pattern

--- practise/one.c
+++ practise/one.c
@@ -1,11 +1,11 @@
 
 #include <stdio.h>
-void invertedTria(int );
+void numTria(int );
 int main(){
   int m;
   printf("Enter the number of rows: ");
   scanf("%d",&m);
-  invertedTria(m);
+  numTria(m);
   
 }
    // RIGHT ANGLED TRIANGLE
@@ -31,3 +31,15 @@ int main(){
   }
 
 
+  //RIGHT ANGLED NUMBER TRIANGLE
+  void numTria(int m ){
+
+    for(int i=1;i<=m;i++){
+      int count =1;
+      for(int j=1;j<=i;j++){
+       printf("%d ",count);
+       count++;
+      }
+      printf("\n");
+    }
+  }
